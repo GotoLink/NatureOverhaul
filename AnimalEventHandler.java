3eef3d57a4738ac3333254582f2e7d2494f6448f
GotoLink <gotolinkminecraft@gmail.com>
2013-06-01 21:48:16 +0200

    Randomized mating behavior.

--- AnimalEventHandler.java
+++ AnimalEventHandler.java
@@ -19,48 +19,45 @@ import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
  *
  */
 public class AnimalEventHandler {
-	private int breedRate,time;
+	private int breedRate;
 	private boolean breed;
 	public AnimalEventHandler(boolean wildAnimalsBreed, int wildAnimalBreedRate) {
 		this.breedRate=wildAnimalBreedRate;
 		this.breed=wildAnimalsBreed;
-		this.time=this.breedRate;
 	}
 
 	@ForgeSubscribe
     public void onLivingUpdateEvent(LivingUpdateEvent event){
-		if(breed && time>0)
-			time--;
-    	if(breed && time==0 && event.entityLiving instanceof EntityAnimal)
+		
+    	if(breed && event.entityLiving instanceof EntityAnimal)
     	{
-    		time = breedRate;
     		EntityAnimal ent =(EntityAnimal)event.entityLiving;
-    		if(!ent.worldObj.isRemote && !ent.isChild()) 
+    		if(!ent.worldObj.isRemote && !ent.isChild() && ent.getGrowingAge()==0) 
     		{
-					EntityAnimal mate=getNearbyMate(ent);
-					if(mate!=null)
-					{
-						EntityAgeable entityageable = ent.createChild(mate);
-				        if (entityageable != null)
-				        {
-				            ent.setGrowingAge(6000);
-				            mate.setGrowingAge(6000);
-				            entityageable.setGrowingAge(-24000);
-				            entityageable.setLocationAndAngles(ent.posX, ent.posY, ent.posZ, 0.0F, 0.0F);
-				            ent.worldObj.spawnEntityInWorld(entityageable);
-				            Random random = ent.getRNG();
-				            for (int i = 0; i < 7; ++i)
-				            {
-				                double d0 = random.nextGaussian() * 0.02D;
-				                double d1 = random.nextGaussian() * 0.02D;
-				                double d2 = random.nextGaussian() * 0.02D;
-				                ent.worldObj.spawnParticle("heart", ent.posX + (double)(random.nextFloat() * ent.width * 2.0F) - (double)ent.width, ent.posY + 0.5D + (double)(random.nextFloat() * ent.height), ent.posZ + (double)(random.nextFloat() * ent.width * 2.0F) - (double)ent.width, d0, d1, d2);
-				            }
-				         }
-					}
-                }
-    		}
-    	}
+				EntityAnimal mate=getNearbyMate(ent);
+				if(mate!=null && new Random().nextFloat() < 1/breedRate)
+				{
+					EntityAgeable entityageable = ent.createChild(mate);
+			        if (entityageable != null)
+			        {
+			            ent.setGrowingAge(6000);
+			            mate.setGrowingAge(6000);
+			            entityageable.setGrowingAge(-24000);
+			            entityageable.setLocationAndAngles(ent.posX, ent.posY, ent.posZ, 0.0F, 0.0F);
+			            ent.worldObj.spawnEntityInWorld(entityageable);
+			            Random random = ent.getRNG();
+			            for (int i = 0; i < 7; ++i)
+			            {
+			                double d0 = random.nextGaussian() * 0.02D;
+			                double d1 = random.nextGaussian() * 0.02D;
+			                double d2 = random.nextGaussian() * 0.02D;
+			                ent.worldObj.spawnParticle("heart", ent.posX + (double)(random.nextFloat() * ent.width * 2.0F) - (double)ent.width, ent.posY + 0.5D + (double)(random.nextFloat() * ent.height), ent.posZ + (double)(random.nextFloat() * ent.width * 2.0F) - (double)ent.width, d0, d1, d2);
+			            }
+			        }
+				}
+        	}
+		}
+	}
     private static EntityAnimal getNearbyMate(EntityAnimal ent) {
     	double d0 = 8.0D;
         List list = ent.worldObj.getEntitiesWithinAABB(ent.getClass(), ent.boundingBox.expand(d0, d0, d0));
