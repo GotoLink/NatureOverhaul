5e8552981ec79c54226dafd8f84f34f5a8c5d786
GotoLink <gotolinkminecraft@gmail.com>
2013-10-03 20:34:51 +0200

    Let log play full breaking animation when...
    ...lumberjacking

--- PlayerEventHandler.java
+++ PlayerEventHandler.java
@@ -3,10 +3,8 @@ package assets.natureoverhaul;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 /**
  * Event for Lumberjack system, from Clinton Alexander idea.
  * @author Olivier
@@ -21,31 +19,24 @@ public class PlayerEventHandler {
 	}
 
 	@ForgeSubscribe
-    public void onPlayerInteracting(PlayerInteractEvent event){
-    	if(lumber && event.action==Action.LEFT_CLICK_BLOCK){
-    		ItemStack itemstack = event.entityPlayer.getCurrentEquippedItem();
+    public void onPlayerHarvesting(HarvestDropsEvent event){
+    	if(lumber && event.harvester != null){
+    		ItemStack itemstack = event.harvester.getCurrentEquippedItem();
     		if(itemstack != null) {
     			Item it = itemstack.getItem();
     			//Check for an axe in player hand
-    			int i=event.x;
-				int j=event.y;
-				int k=event.z;
-    			if(it!=null && it instanceof ItemAxe && !it.onBlockStartBreak(itemstack, i, j, k, event.entityPlayer)){
-    				int id=event.entityPlayer.worldObj.getBlockId(i, j, k);
+    			if(it instanceof ItemAxe){
+    				int id=event.block.blockID;
     				//Check for a registered log block
-    				if(NatureOverhaul.instance.isLog(id) ){
-    					World world=event.entityPlayer.worldObj;
-    					
-    					if( TreeUtils.isTree(world, i, j, k, Utils.getType(id), true) )
-    					{
-    						int meta = world.getBlockMetadata(i, j, k);
+    				if(NatureOverhaul.instance.isLog(id)){
+    					if(TreeUtils.isTree(event.world, event.x, event.y, event.z, Utils.getType(id), true)){
     						// Damage axe compared to the number of blocks found
-					  		int damage = TreeUtils.killTree(world, i, j, k, id, leafKill);
-        					itemstack.damageItem(damage - 1, event.entityPlayer);
+					  		int damage = TreeUtils.killTree(event.world, event.x, event.y, event.z, id, leafKill);
+        					itemstack.damageItem(damage - 1, event.harvester);
         					if(itemstack.stackSize <= 0) 
-        						event.entityPlayer.destroyCurrentEquippedItem();
+        						event.harvester.destroyCurrentEquippedItem();
         					//Drop logs
-        					Utils.emitItem(world, i, j, k, new ItemStack(id, damage, meta));
+        					Utils.emitItem(event.world, event.x, event.y, event.z, new ItemStack(id, damage, event.blockMetadata));
     					}
     				}
     			}
