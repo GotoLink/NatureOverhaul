b388a420ad138d24ddd51f03394a6f195fa09abd
GotoLink <gotolinkminecraft@gmail.com>
2014-04-25 21:36:39 +0200

    Upgrade moss behavior to be more API oriented

--- natureoverhaul/behaviors/BehaviorMoss.java
+++ natureoverhaul/behaviors/BehaviorMoss.java
@@ -5,11 +5,35 @@ import natureoverhaul.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class BehaviorMoss extends BehaviorDeathSwitch {
-	@Override
-	public Block getDeadBlock() {
-		return Blocks.cobblestone;
+    private static Block[] mossy = {Blocks.mossy_cobblestone, Blocks.stonebrick};
+    private static Block[] smooth = {Blocks.cobblestone, Blocks.stonebrick};
+    private static int[] mossMeta = {0, 1};
+
+    public static void addMossData(Block moss, Block norm, int meta){
+        ArrayUtils.add(mossy, moss);
+        ArrayUtils.add(smooth, norm);
+        ArrayUtils.add(mossMeta, meta);
+    }
+
+    public static boolean isMossyBlock(Block test){
+        for(Block block:mossy){
+            if(block==test){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+	public Block getDeadBlock(Block living) {
+		for(int i=0; i<mossy.length; i++){
+            if(living==mossy[i]){
+                return smooth[i];
+            }
+        }
+        return null;
 	}
 
 	@Override
@@ -20,16 +44,30 @@ public class BehaviorMoss extends BehaviorDeathSwitch {
 	@Override
 	public void grow(World world, int i, int j, int k, Block id) {
 		//Moss grows on both stone (or only cobblestone), changing only one block
-		int scanSize = 1;
-		Block iD;
-		int coord[];
-		for (int attempt = 0; attempt < 15; attempt++) {
-			coord = Utils.findRandomNeighbour(i, j, k, scanSize);
-			iD = world.getBlock(coord[0], coord[1], coord[2]);
-			if ((NatureOverhaul.mossCorruptStone && iD == Blocks.stone) || iD == getDeadBlock()) {
-				world.setBlock(coord[0], coord[1], coord[2], id);
-				return;
-			}
-		}
+		if(world.getBlockMetadata(i, j, k) == getGrowthMeta(id)) {
+            Block iD;
+            int coord[];
+            for (int attempt = 0; attempt < 15; attempt++) {
+                coord = Utils.findRandomNeighbour(i, j, k, 1);
+                iD = world.getBlock(coord[0], coord[1], coord[2]);
+                if (canGrowOn(id, iD)) {
+                    world.setBlock(coord[0], coord[1], coord[2], id, getGrowthMeta(id), 3);
+                    return;
+                }
+            }
+        }
 	}
+
+    public boolean canGrowOn(Block living, Block area){
+        return (NatureOverhaul.mossCorruptStone && area == Blocks.stone && living == mossy[0]) || area == getDeadBlock(living);
+    }
+
+    public int getGrowthMeta(Block living){
+        for(int i=0; i<mossy.length; i++){
+            if(living==mossy[i]){
+                return mossMeta[i];
+            }
+        }
+        return -1;
+    }
 }
